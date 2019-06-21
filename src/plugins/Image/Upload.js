5e7d6069b6383924fc352cb393256fc2082af02d
Daniel Soares <danielpfsoares@gmail.com>
2019-06-21 02:41:18 -0300

    Corrigindo upload de imagem

--- src/plugins/Image/Upload.js
+++ src/plugins/Image/Upload.js
@@ -1,71 +1,75 @@
-import Plugin from '@/contracts/Plugin'
-import Button from '@/contracts/Button'
+import Plugin from "@/contracts/Plugin";
+import Button from "@/contracts/Button";
 
 export default class Upload extends Plugin {
-  regex = /file:\/\/\/[\w|\W]*/
-  buttons = [
-    new Button('uploadImage', 'upload.new', 'add_a_photo')
-  ]
-  
-  constructor(core) {
-    super(core)
+  regex = /file:\/\/\/[\w|\W]*/;
+  buttons = [new Button("uploadImage", "upload.new", "add_a_photo")];
 
-    this.core.editor.addEventListener('drop', this._onDrop.bind(this))
+  constructor(core) {
+    super(core);
+
+    this.core.editor.addEventListener("drop", this._onDrop.bind(this));
   }
 
   onPaste(event) {
-    let items = event.clipboardData.items ? (items => {
-      let tmp = []
-      for (let i = 0; i < items.length; i++) {
-        if (items[i].kind === 'file')
-          tmp.push(items[i])
-      }
-      return tmp
-    })(event.clipboardData.items) : event.clipboardData.files
+    let items = event.clipboardData.items
+      ? (items => {
+          let tmp = [];
+          for (let i = 0; i < items.length; i++) {
+            if (items[i].kind === "file") tmp.push(items[i]);
+          }
+          return tmp;
+        })(event.clipboardData.items)
+      : event.clipboardData.files;
 
-    if (items.length <= 0)
-      return
-    
-    this._insertImages(items)
+    if (items.length <= 0) return;
+
+    event.preventDefault();
+    this._insertImages(items);
   }
-    
+
   _onDrop(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    let items = event.dataTransfer.items ? (items => {
-      let tmp = []
-      for (let i = 0; i < items.length; i++) {
-        if (items[i].kind === 'file')
-          tmp.push(items[i])
-      }
-      return tmp
-    })(event.dataTransfer.items) : event.dataTransfer.files
+    let items = event.dataTransfer.items
+      ? (items => {
+          let tmp = [];
+          for (let i = 0; i < items.length; i++) {
+            if (items[i].kind === "file") tmp.push(items[i]);
+          }
+          return tmp;
+        })(event.dataTransfer.items)
+      : event.dataTransfer.files;
 
-    if (items.length <= 0)
-      return
-    
-    this._insertImages(items)
+    if (items.length <= 0) return;
+
+    this._insertImages(items);
   }
 
   _insertImages(files) {
     // lendo arquivos
     for (let i = 0; i < files.length; i++) {
-      let reader = new FileReader()
+      let reader = new FileReader();
       reader.onloadend = function() {
-        document.execCommand('insertImage', false, reader.result)
-      }
-      reader.readAsDataURL(files[i].getAsFile ? files[i].getAsFile() : files[i])
+        document.execCommand("insertImage", false, reader.result);
+      };
+      reader.readAsDataURL(
+        files[i].getAsFile ? files[i].getAsFile() : files[i]
+      );
     }
   }
 
   new() {
-    let input = document.createElement('input')
-    input.type = 'file'
-    f.accept = 'image/*'
-    input.multiple = true
-    input.addEventListener('change', ((event) => {
-      this._insertImages(event.path[0].files)
-    }).bind(this))
-    input.click()
+    let input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
+    input.multiple = true;
+    input.addEventListener(
+      "change",
+      (event => {
+        this._insertImages(event.target.files || event.path[0].files);
+      }).bind(this)
+    );
+    input.click();
   }
 }
