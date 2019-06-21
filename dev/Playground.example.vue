7cbc2b93c44bb03118641fb007316395b29cd3fa
Daniel Soares <daniel.soares@grupozago.com.br>
2019-06-21 16:38:18 -0300

    Teste de renderizacao

--- dev/Playground.example.vue
+++ dev/Playground.example.vue
@@ -1,16 +1,43 @@
 <template>
-    <div>
-        <h1>Editor's Ivuup</h1>
-        <editor></editor>
-    </div>
+  <div class="container">
+    <h1>Editor's Ivuup</h1>
+    <editor ref="editor"></editor>
+    <v-btn @click="mountComponent">render</v-btn>
+    <fake-data class="mt-5" :test="test"></fake-data>
+    <v-text-field v-model="test"></v-text-field>
+  </div>
 </template>
 
 <script>
-import Editor from '@/dist/index.vue'
+import Vue from "vue";
+import Editor from "@/index.vue";
+import FakeData from "./components/FakeData.vue";
 
 export default {
-    components: {
-        Editor
+  components: {
+    Editor,
+    FakeData
+  },
+  data() {
+    return {
+      test: 20
+    };
+  },
+  methods: {
+    mountComponent() {
+      new Vue({
+        el: this.$refs.editor.$refs.editor.core.editor.firstChild,
+        render: h =>
+          h(FakeData, {
+            domProps: {
+              contentEditable: false
+            },
+            props: {
+              test: this.test
+            }
+          })
+      });
     }
-}
+  }
+};
 </script>
---
+++ dev/components/FakeData.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title class="title">{{ test }}</v-card-title>
+    <v-card-text
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo fugiat
+      saepe consectetur tempora optio, iusto neque natus impedit quasi aperiam
+      expedita dolores non? Maxime, quaerat exercitationem provident a modi
+      perferendis.</v-card-text
+    >
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    test: {
+      default: "Original"
+    }
+  }
+};
+</script>
