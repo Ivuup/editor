dfa4e2d1130655f98656a03497c5ce437f287bb8
Daniel Soares <danielpfsoares@gmail.com>
2019-10-11 17:17:36 -0300

    atualizando vuetify

--- dev/main.js
+++ dev/main.js
@@ -1,15 +1,17 @@
-import Vue from "vue"
-import App from "./App.vue"
-import router from "./router"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 // Vuetify - remover dependência
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-Vue.use(Vuetify)
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify: new Vuetify({}),
   router,
   render: h => h(App)
 }).$mount("#app");
